58601f1fff1981221693f71e2c8f26c8b4672cc1
ShlokMarne <95908743+ShlokMarne@users.noreply.github.com>
2024-08-29 22:50:14 +0530

    Add files via upload

---
+++ operators1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float a = 14.0,b = 5.0;
+    printf("%f + %f = %f\n",a,b,a + b);
+    printf("%f - %f = %f\n",a,b,a - b);
+    printf("%f * %f = %f\n",a,b,a * b);
+    printf("%f / %f = %f\n",a,b,a / b);
+    return 0;
+}
