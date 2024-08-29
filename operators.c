e3f4447fe48b4dc16c190b74d1069927ada95106
ShlokMarne <95908743+ShlokMarne@users.noreply.github.com>
2024-08-29 22:49:11 +0530

    Program using operators

---
+++ operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a = 14,b = 2;
+    printf("%d + %d = %d\n",a,b,a + b);
+    printf("%d - %d = %d\n",a,b,a - b);
+    printf("%d * %d = %d\n",a,b,a * b);
+    printf("%d / %d = %d\n",a,b,a / b);
+    printf("%d %% %d = %d\n",a,b,a % b);
+    return 0;
+}
