4e4d1b2f089c8cd4de2abea8aff42e316331806d
ShlokMarne <95908743+ShlokMarne@users.noreply.github.com>
2024-08-29 15:16:45 +0530

    Typecast Program

---
+++ typecast.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10,sum;
+    char c = 's';
+     printf("Character C is %c\n",c);
+     printf("Numeric C is %d\n",(int)c);
+    sum = a + (int)c;
+    printf("Sum = %d\n",sum);
+    return 0;
+}
