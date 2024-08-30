ddadface73d79ee59a7d490d95de94c14a1dd565
ShlokMarne <95908743+ShlokMarne@users.noreply.github.com>
2024-08-30 15:58:48 +0530

    C Program to Print the ASCII Value of a Character

---
+++ asciivalue.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char c;
+    printf("Enter character to get ASCII Value: ");
+    scanf("%c",&c);
+    printf("The ASCII value of %c is %d ",c,c);
+    return 0;
+}
