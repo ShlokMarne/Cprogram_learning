d49cf5e9817c4a5116983693a5f663d5591ffff7
ShlokMarne <95908743+ShlokMarne@users.noreply.github.com>
2024-09-02 14:00:10 +0530

    C Program to Calculate Sum of Natural Numbers

---
+++ sum-of-natural-number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i,s = 0;
+    int n = 11;
+    i = 1;
+     while (i <= n)
+     {
+        s += 1;
+        i++;
+     }
+     printf("Sum = %d",s);
+     return 0;
+}
