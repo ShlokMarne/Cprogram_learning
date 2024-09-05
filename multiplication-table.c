56185a84916a83b1f4f363fd19581ad2edfe6f41
ShlokMarne <95908743+ShlokMarne@users.noreply.github.com>
2024-09-05 11:57:04 +0530

    C Program to Generate Multiplication Table

---
+++ multiplication-table.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void print_table(int range, int num)
+{
+    int mul, i;
+    for (i = 1; i <= range; i++)
+    {
+        mul = num * i;
+        printf("%d * %d = %d",num,i,mul);
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int num,range;
+    printf("Enter table: ",num);
+    scanf("%d",&num);
+    printf("Enter range: ",range);
+    scanf("%d",&range);
+    print_table(range,num);
+    return 0;
+}
